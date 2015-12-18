d6d75fd48a7d7e8b8ba3a920b2f5b402cffcb8c1
JoelAlphonso <admin@joeldesign.ca>
2015-12-18 14:53:26 -0500

    ajout arts
    Ajout décor, day/night cycle, nuages, montaignes et animation wiggle

--- wp-content/themes/portfolio - sidescroller/index.php
+++ wp-content/themes/portfolio - sidescroller/index.php
@@ -21,37 +21,62 @@
 
 <ul id="scene">
     <li class="layer" data-depth="0.1">
-
+        <div>
+            <div class="day-night">
+                <div class="nuit"></div>
+                <div class="jour"></div>
+            </div>
+        </div>
+        
     </li>
     <li class="layer" data-depth="0.2">
-        
+        <div class="paraLayerContainer">
+            <div class="paraLayer horizon" data-depth="0.2"></div>
+            <div class="paraLayer wiggle nuages-back" data-depth="0.2"></div>
+        </div>
     </li>
     <li class="layer" data-depth="0.3">
-        
+        <div class="paraLayerContainer">
+            <div class="paraLayer mont-back" data-depth="0.3"></div>
+            <div class="paraLayer wiggle nuages-middle" data-depth="0.3"></div>
+        </div>
     </li>
     <li class="layer" data-depth="0.4">
-        
+        <div class="paraLayerContainer">
+            <div class="paraLayer mont-middle" data-depth="0.4"></div>
+            <div class="paraLayer wiggle nuages-front" data-depth="0.3"></div>
+        </div>
     </li>
     <li class="layer" data-depth="0.5">
-        
+        <div class="paraLayerContainer">
+            <div class="paraLayer mont-front" data-depth="0.5"></div>
+        </div>
     </li>
     <li class="layer" data-depth="0.6">
-        
+        <div class="paraLayerContainer">
+            <div class="paraLayer" data-depth="0.9"></div>
+        </div>
     </li>
     <li class="layer" data-depth="0.7">
-        
+        <div class="paraLayerContainer">
+            <div class="paraLayer mainLayer sol" data-depth="1">
+                
+            </div>
+        </div>
     </li>
     <li class="layer" data-depth="0.8">
         
     </li>
     <li class="layer" data-depth="0.9">
-        
-    </li>
-    <li class="layer" data-depth="1">
         <div class="paraLayerContainer">
-            <div class="paraLayer" data-depth="1"></div>
+            <div class="paraLayer sous-sol" data-depth="1.1"></div>
         </div>
     </li>
+    <li class="layer" data-depth="1">
+        
+    </li>
+
 </ul>
+<div class="ciel"></div>
 <?php get_footer(); ?>
 </body>
--- wp-content/themes/portfolio - sidescroller/js/script.js
+++ wp-content/themes/portfolio - sidescroller/js/script.js
@@ -13,6 +13,14 @@ $(document).ready(function ($) {
     // Side Scroller initialisation
     sideScrollerInit();
 
+    // DayNight cycle
+    dayNightCycle();
+
+    $(window).resize(function(event) {
+        /* Act on the event */
+        updateSideScroller();
+    });
+
     // Handle smooth-scroll
     $('nav .cd-primary-nav a').smoothScroll({
         afterScroll: function () {
@@ -72,15 +80,15 @@ $(document).ready(function ($) {
 
 function wiggle(selector) {
     $(selector).each(function () {
-        wiggleProp(this, 'scale', 0.85, 1);
-        wiggleProp(this, 'rotation', -5, 5);
+        wiggleProp(this, 'scale', 1, 1);
+        wiggleProp(this, 'rotation', 0, 0);
         wiggleProp(this, 'x', -10, 10);
         wiggleProp(this, 'y', -10, 10);
     });
 }
 
 function wiggleProp(element, prop, min, max) {
-    var duration = Math.random() * 2 + 1;
+    var duration = Math.random() * 2 + 2;
 
     var tweenProps = {
         ease: Power1.easeInOut,
@@ -154,6 +162,9 @@ function sideScrollerInit(){
     },
     layers = [],
     keys = [],
+    min = $(".mainLayer").position().left,
+    max = $(window).width() * 1.5 - $(".mainLayer").width(),
+    mainLayerWidth = $(".mainLayer").width(),
     friction = 0.95;
 
     // Get all layers in the table
@@ -162,12 +173,21 @@ function sideScrollerInit(){
         var currentLayer = $(this);
         var layerPos = currentLayer.position().left;
         var layerSpeed = currentLayer.data('depth');
+        var width = mainLayerWidth + (mainLayerWidth * layerSpeed / mainLayerWidth)
 
         layers[index] = {
             layer: currentLayer,
             position: layerPos,
             speed: layerSpeed
         };
+
+        // Size the layers
+        if(layerSpeed < 1){
+            currentLayer.width($('.mainLayer').width() * layerSpeed + $(window).width() * 1.5);
+        }else{
+            currentLayer.width($('.mainLayer').width() * layerSpeed);
+        }
+
     });
 
     $(document).keydown(function(event) {
@@ -188,14 +208,22 @@ function updateFrame(){
     
     if(keys[39]){ 
         // Right Arrow
-        if(sideScroller.velocity > -sideScroller.speed){
-            sideScroller.velocity--;
+        if($(".mainLayer").position().left >= max){
+            if(sideScroller.velocity > -sideScroller.speed){
+                sideScroller.velocity--;
+            }
+        }else{
+            sideScroller.velocity = 0;
         }
     }
     if(keys[37]){
         // Left Arrow
-        if(sideScroller.velocity < sideScroller.speed){
-            sideScroller.velocity++;
+        if($(".mainLayer").position().left <= min){
+            if(sideScroller.velocity < sideScroller.speed){
+                sideScroller.velocity++;
+            }
+        }else{
+            sideScroller.velocity = 0;
         }
     }
 
@@ -203,19 +231,57 @@ function updateFrame(){
     sideScroller.velocity *= friction;
 
     // Apply movement to each layers
-    $.each(layers, function(index, layer){
-        
+
+    // $.each(layers, function(index, layer){
+    for(var layer in layers){ 
         //console.log(sideScroller.velocity);
         // Update object data
-        layer.position += (sideScroller.velocity * layer.speed);
+        layers[layer].position += (sideScroller.velocity * layers[layer].speed);
 
         // The new position in pixels
-        var xPos = layer.position + "px";
+        var xPos = layers[layer].position + "px";
 
         // Apply transformation
-        layer.layer.css("transform", "translateX("+ xPos +")");
+        layers[layer].layer.css("transform", "translateX("+ xPos +")");
 
-    });
+    };
+}
+
+function updateSideScroller(){
+    min = $(".mainLayer").position().left;
+    max = $(window).width() * 1.5 - $(".mainLayer").width();
+}
+
+function dayNightCycle(){
+    
+    var time = $(".day-night");
+    var ciel = $(".ciel");
+    var day = $(".jour");
+
+    TweenMax.from(time, 0, { rotation:0, repeat:-1, ease:Linear.easeNone } );
+    TweenMax.to(time, 120, { rotation:360, repeat:-1, ease:Linear.easeNone } );
+
+    var animation = new TimelineLite({repeat:-1});
+    
+    animation.to(ciel, 20, { backgroundColor:"#d9c72b", ease:Linear.easeNone } )
+        .to(ciel, 10, { backgroundColor:"#d9552b", ease:Linear.easeNone } )
+        .to(ciel, 10, { backgroundColor:"#6e10e8", ease:Linear.easeNone } )
+        .to(ciel, 40, { backgroundColor:"#6e10e8", ease:Linear.easeNone } )
+        .to(ciel, 10, { backgroundColor:"#e86f1c", ease:Linear.easeNone } )
+        .to(ciel, 10, { backgroundColor:"#d9c72b", ease:Linear.easeNone } )
+        .to(ciel, 20, { backgroundColor:"#d9c72b", ease:Linear.easeNone } );
+
+    animation.play();
+
+    var animation2 = new TimelineLite({repeat:-1});
+
+    animation2.to(day, 30, { opacity:1, ease:Linear.easeNone } )
+        .to(day, 10, { opacity:0, ease:Linear.easeNone } )
+        .to(day, 40, { opacity:0, ease:Linear.easeNone } )
+        .to(day, 10, { opacity:1, ease:Linear.easeNone } )
+        .to(day, 30, { opacity:1, ease:Linear.easeNone } );
+
+    animation2.play();
 }
 
 })(jQuery);
